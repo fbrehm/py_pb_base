64df7bd75078302f97e3963093f131c002b2e07c
Frank Brehm <frank.brehm@profitbricks.com>
2012-10-09 11:16:28 +0200

    Added pb_base/__init__.py

---
+++ pb_base/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@copyright: (c) 2010 - 2012 by Frank Brehm, Berlin
+@summary: All modules for Python base object and error classes
+"""
+
+__author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
+__copyright__ = '(C) 2010-2012 by profitbricks.com'
+__contact__ = 'frank.brehm@profitbricks.com'
+__version__ = '0.1.0-1'
+__license__ = 'GPL3'
+
+# vim: fileencoding=utf-8 filetype=python ts=4
