1b7bdf06f09080d178af475bded74b801005dada
Frank Brehm <frank.brehm@profitbricks.com>
2012-10-11 17:47:45 +0200

    Added argparsing to pb_base/app.py

--- pb_base/app.py
+++ pb_base/app.py
@@ -16,7 +16,10 @@ import sys
 import os
 import logging
 
+from gettext import gettext as _
+
 # Third party modules
+import argparse
 
 # Own modules
 from pb_base.common import pp
@@ -32,7 +35,7 @@ from pb_base.object import PbBaseObject
 __author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
 __copyright__ = '(C) 2010-2012 by profitbricks.com'
 __contact__ = 'frank.brehm@profitbricks.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __license__ = 'GPL3'
 
 log = logging.getLogger(__name__)
@@ -58,6 +61,10 @@ class PbApplication(PbBaseObject):
                 base_dir = None,
                 use_stderr = False,
                 initialized = False,
+                usage = None,
+                description = None,
+                argparse_epilog = None,
+                argparse_prefix_chars = '-',
                 ):
         '''
         Initialisation of the base object.
@@ -79,6 +86,16 @@ class PbApplication(PbBaseObject):
         @param initialized: initialisation is complete after __init__()
                             of this object
         @type initialized: bool
+        @param usage: usage text used on argparse
+        @type usage: str
+        @param description: a short text describing the application
+        @type description: str
+        @param argparse_epilog: an epilog displayed at the end
+                                of the argparse help screen
+        @type argparse_epilog: str
+        @param argparse_prefix_chars: The set of characters that prefix
+                                      optional arguments.
+        @type argparse_prefix_chars: str
 
         @return: None
         '''
@@ -92,6 +109,12 @@ class PbApplication(PbBaseObject):
                 initialized = False,
         )
 
+        self.arg_parser = None
+        """
+        @ivar: argparser object to parse commandline parameters
+        @type: argparse.ArgumentParser
+        """
+
         self.args = None
         """
         @ivar: an object containing all commandline parameters
@@ -104,6 +127,33 @@ class PbApplication(PbBaseObject):
         @ivar: return value of the application for exiting with sys.exit().
         @type: int
         """
+
+        self._usage = usage
+        """
+        @ivar: usage text used on argparse
+        @type: str
+        """
+
+        self._description = description
+        """
+        @ivar: a short text describing the application
+        @type: str
+        """
+
+        self._argparse_epilog = argparse_epilog
+        """
+        @ivar: an epilog displayed at the end of the argparse help screen
+        @type: str
+        """
+
+        self._argparse_prefix_chars = argparse_prefix_chars
+        """
+        @ivar: The set of characters that prefix optional arguments.
+        @type: str
+        """
+
+        self._init_arg_parser()
+        self._perform_arg_parser()
 
         self.init_logging()
 
@@ -122,6 +172,54 @@ class PbApplication(PbBaseObject):
                 self._exit_value = v
             else:
                 log.warn("Wrong exit_value %r, must be >= 0", value)
+        def fdel(self):
+            pass
+        return property(**locals())
+
+    #------------------------------------------------------------
+    @apply
+    def usage():
+        doc = "The usage text used on argparse."
+        def fget(self):
+            return self._usage
+        def fset(self, value):
+            pass
+        def fdel(self):
+            pass
+        return property(**locals())
+
+    #------------------------------------------------------------
+    @apply
+    def description():
+        doc = "A short text describing the application."
+        def fget(self):
+            return self._description
+        def fset(self, value):
+            pass
+        def fdel(self):
+            pass
+        return property(**locals())
+
+    #------------------------------------------------------------
+    @apply
+    def argparse_epilog():
+        doc = "An epilog displayed at the end of the argparse help screen."
+        def fget(self):
+            return self._argparse_epilog
+        def fset(self, value):
+            pass
+        def fdel(self):
+            pass
+        return property(**locals())
+
+    #------------------------------------------------------------
+    @apply
+    def argparse_prefix_chars():
+        doc = "The set of characters that prefix optional arguments."
+        def fget(self):
+            return self._argparse_prefix_chars
+        def fset(self, value):
+            pass
         def fdel(self):
             pass
         return property(**locals())
@@ -147,7 +245,7 @@ class PbApplication(PbBaseObject):
             if self.verbose > 1:
                 format_str += '%(name)s(%(lineno)d) %(funcName)s() '
             else:
-                format_str += '%(name)s'
+                format_str += '%(name)s '
         format_str += '%(levelname)s - %(message)s'
         color_formatter = ColoredFormatter(format_str)
 
@@ -229,6 +327,83 @@ class PbApplication(PbBaseObject):
 
         if self.verbose > 1:
             log.info("executing post_run() ...")
+
+    #--------------------------------------------------------------------------
+    def _init_arg_parser(self):
+        """
+        Local called method to initiate the argument parser.
+
+        @raise PbApplicationError: on some errors
+
+        """
+
+        self.arg_parser = argparse.ArgumentParser(
+            prog = self.appname,
+            description = self.description,
+            usage = self.usage,
+            epilog = self.argparse_epilog,
+            prefix_chars = self.argparse_prefix_chars,
+            add_help = False,
+        )
+
+        self.init_arg_parser()
+
+        general_group = self.arg_parser.add_argument_group('General options:')
+        general_group.add_argument(
+                "-v", "--verbose",
+                action = "count",
+                dest = 'verbose',
+                help = _('Increase the verbosity level'),
+        )
+        general_group.add_argument(
+                "-h", "--help",
+                action = 'help',
+                dest = 'help',
+                help = _('Show this help message and exit')
+        )
+        general_group.add_argument(
+                "--usage",
+                action = 'store_true',
+                dest = 'usage',
+                help = _("Display brief usage message and exit")
+        )
+        general_group.add_argument(
+                "-V", "--version",
+                action = 'store_true',
+                dest = 'version',
+                help = _("Show program's version number and exit")
+        )
+
+    #--------------------------------------------------------------------------
+    def init_arg_parser(self):
+        """
+        Public available method to initiate the argument parser.
+        Descendant classes may override this method.
+        """
+
+        pass
+
+    #--------------------------------------------------------------------------
+    def _perform_arg_parser(self):
+        """
+        Underlaying method for parsing arguments.
+        """
+
+        self.args = self.arg_parser.parse_args()
+
+        if self.args.version:
+            msg = self.version
+            if self.args.verbose:
+                msg = _("Version of %s: %s") % (self.appname, self.version)
+            sys.stdout.write(msg + "\n")
+            sys.exit(0)
+
+        if self.args.usage:
+            self.arg_parser.print_usage(sys.stdout)
+            sys.exit(0)
+
+        if self.args.verbose > self.verbose:
+            self.verbose = self.args.verbose
 
 #==============================================================================
 
