5182281911e224af69e4d2a9b3e3a7cf3a821b9f
Frank Brehm <frank.brehm@profitbricks.com>
2013-02-04 11:52:33 +0100

    Started with test/test_cryptpass.py as a testing module for pb_base.cryptpass

---
+++ test/test_cryptpass.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@organization: Profitbricks GmbH
+@copyright: © 2010 - 2013 by Profitbricks GmbH
+@license: GPL3
+@summary: test script (and module) for unit tests on the cryptpass module
+'''
+
+import unittest
+import os
+import sys
+import logging
+
+libdir = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+sys.path.insert(0, libdir)
+
+import general
+from general import PbBaseTestcase, get_arg_verbose, init_root_logger
+
+from pb_base.cryptpass import gensalt, shadowcrypt
+
+log = logging.getLogger(__name__)
+
+#==============================================================================
+
+if __name__ == '__main__':
+
+    verbose = get_arg_verbose()
+    init_root_logger(verbose)
+
+    log.info("Starting tests ...")
+
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    runner = unittest.TextTestRunner(verbosity = verbose)
+
+    result = runner.run(suite)
+
+
+#==============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
