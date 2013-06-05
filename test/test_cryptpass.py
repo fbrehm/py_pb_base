0a4a419a0f2c3ea1321d8b62667c7d464c3dd808
Frank Brehm <frank.brehm@profitbricks.com>
2013-06-05 11:46:35 +0200

    Bugfixing in test scripts

--- test/test_cryptpass.py
+++ test/test_cryptpass.py
@@ -107,6 +107,8 @@ class TestCryptPass(PbBaseTestcase):
 if __name__ == '__main__':
 
     verbose = get_arg_verbose()
+    if verbose is None:
+        verbose = 0
     init_root_logger(verbose)
 
     log.info("Starting tests ...")
