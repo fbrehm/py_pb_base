ab21fb2105e3bf28d980ec006bdfa1e58bdf2635
Frank Brehm <frank.brehm@profitbricks.com>
2013-06-29 22:12:50 +0200

    Added test/test_common.py

---
+++ test/test_common.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@organization: Profitbricks GmbH
+@copyright: © 2010 - 2013 by Profitbricks GmbH
+@license: GPL3
+@summary: test script (and module) for unit tests on common.py
+"""
+
+import unittest
+import os
+import sys
+import logging
+
+libdir = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+sys.path.insert(0, libdir)
+
+import general
+from general import PbBaseTestcase, get_arg_verbose, init_root_logger
+
+log = logging.getLogger(__name__)
+
+#==============================================================================
+
+class TestPbCommon(PbBaseTestcase):
+
+    #--------------------------------------------------------------------------
+    def setUp(self):
+        pass
+
+    #--------------------------------------------------------------------------
+    def test_import(self):
+
+        log.info("Testing import of pb_base.common ...")
+        import pb_base.object
+
+#==============================================================================
+
+if __name__ == '__main__':
+
+    verbose = get_arg_verbose()
+    if verbose is None:
+        verbose = 0
+    init_root_logger(verbose)
+
+    log.info("Starting tests ...")
+
+    suite = unittest.TestSuite()
+
+    suite.addTest(TestPbCommon('test_import', verbose))
+
+    runner = unittest.TextTestRunner(verbosity = verbose)
+
+    result = runner.run(suite)
+
+#==============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
