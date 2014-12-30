bc3d3b10e3697639fcfb1212595045b99eddf504
Frank Brehm <frank.brehm@profitbricks.com>
2014-12-30 15:30:37 +0100

    Redefined translation methods

--- pb_base/translate.py
+++ pb_base/translate.py
@@ -15,6 +15,9 @@ import os
 import logging
 import gettext
 
+# Own modules
+from pb_base.common import to_str_or_bust
+
 log = logging.getLogger(__name__)
 
 basedir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
@@ -30,15 +33,29 @@ The main gettext-translator object, which can be imported
 from other modules.
 """
 
-_ = translator.lgettext
-__ = translator.lngettext
+
+# =============================================================================
+def pb_gettext(message):
+    if sys.version_info[0] > 2:
+        return to_str_or_bust(translator.gettext(message))
+    else:
+        return to_str_or_bust(translator.lgettext(message))
+
+
+# =============================================================================
+def pb_ngettext(singular, plural, n):
+    if sys.version_info[0] > 2:
+        return to_str_or_bust(translator.ngettext(singular, plural, n))
+    else:
+        return to_str_or_bust(translator.lngettext(singular, plural, n))
+
 
 # =============================================================================
 
 if __name__ == "__main__":
 
-    print(_("Basedir: %r") % (basedir))
-    print(_("Found .mo-file: %r") % (mo_file))
+    print(pb_lgettext("Basedir: %r") % (basedir))
+    print(pb_lgettext("Found .mo-file: %r") % (mo_file))
 
 # =============================================================================
 
